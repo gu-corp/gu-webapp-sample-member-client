54fc9668cd46f6748be4af2f0955ff485ce2395a
Hidekazu Kondo <kaz@gulabs.com>
2021-05-19 13:25:25 +0900

    fix: valudator-helper bug

--- src/common/validator/class-validator-helper.ts
+++ src/common/validator/class-validator-helper.ts
@@ -3,15 +3,14 @@ import { i18n } from 'next-i18next'
 
 export async function validateObject(object) {
   const errors = await validate(object);
-  // errors is an array of validation errors
-  if (errors.length > 0) {
   const resultErrors = {};
+  // errors is an array of validation errors
+  if (errors.length > 0) {    
     errors.forEach(error => {
-      // console.log(error);
       const keys = Object.keys(error.constraints);
       const errorString = keys[0];
       resultErrors[error.property] = i18n.t(error.constraints[errorString]);
-    })                    
-    return resultErrors;
+    })                       
   } 
+  return resultErrors;
 }
